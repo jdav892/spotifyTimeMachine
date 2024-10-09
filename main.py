6aa87ce2cae3e6a93a02eea04cd813a30ce1e64a
jdav892 <jadavila9@gmail.com>
2024-10-09 18:20:30 -0400

    adds feature to create playlist in spotify for authorized users based on date they submit to generate it

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+import pprint
 
 load_dotenv(f"C:/Users/jay-5/Documents/code/pythonProj/timeMachine/api.env")
 CLIENT_ID = os.environ.get("CLIENT_ID")
@@ -18,7 +19,6 @@ sp = spotipy.Spotify(
     client_secret=CLIENT_SECRET,
     show_dialog=True,
     cache_path="token.txt",
-    username=USERNAME,
     )
 )
 user_id = sp.current_user()["id"]
@@ -31,32 +31,44 @@ date = input("What time would you like to travel to ?(In YYYY-MM-DD format) ")
 
 URL = "https://www.billboard.com/charts/hot-100/" + date
 
-names = ["List of song", "Titles", "scrape"]
 
-song_uris=[]
-
-year = date.split("-")[0]
-for song in names:
-    result = sp.search(q=f"track:{song} year:{year}", type="track")
-    try:
-        uri = result["tracks"]["items"][0]["uri"]
-        song_uris.append(uri)
-    except IndexError:
-        print(f"{song} doesn't exist in Spotify. Skipped.")
-    
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
 }
 
 response = requests.get(URL, headers=headers)
 
-if response.status_code == 200:
-    url_page = response.text
-    soup = BeautifulSoup(url_page, 'html.parser')
 
-    song_list = soup.select("li ul li h3")
-    song_names = [song.getText().strip() for song in song_list]
+url_page = response.text
+soup = BeautifulSoup(url_page, 'html.parser')
+song_list = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_list]
 
-    print(song_names)
+    
+names = ["List of song", "Titles", "scrape"]
+
+song_uris=[]
+
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    if result["tracks"]["items"]:
+        song_uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(song_uri)
+    else:
+        print(f"Song: {song} does not exist on Spotify.")
+
+playlist_name = f"Billboard Hot 100 - {date}" 
+description = "Billboard Hot 100 for the year specified by user."
+
+playlist = sp.user_playlist_create(user=user_id,
+                                   name=playlist_name,
+                                   description=description,
+                                   public=False)
+
+if song_uris:
+    sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+    print(f"Playlist '{playlist_name}' created successfully.")
 else:
-    print("Failed to retrieve page Status Code: ", response.status_code)   
+    print("No songs found to add to the playlist.")
+    
